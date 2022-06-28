18f3d5276acf9c3402c596d6f7f5c427579265d4
Oopas <69417818+Oopas@users.noreply.github.com>
2022-06-28 10:21:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,6 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+cd package && git clone https://github.com/maxlicheng/luci-app-unblockmusic.git ; cd ..
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
